ebc0fce2667a77a95337b28de6fd265d36d9b36a
SergioBernardo <sergiobernardo@msn.com>
2021-07-13 22:02:40 -0300

    continuação com set, list array

---
+++ src/main/kotlin/one/digitalinnovetion/collections/TesteArrayString.kt
@@ -0,0 +1,22 @@
+package one.digitalinnovetion.collections
+
+fun main(){
+    val nomes = Array<String>(3){""}
+    nomes[0] = "Maria"
+    nomes[1] = "João"
+    nomes[2] = "José"
+
+    for (nome in nomes){
+        println(nome)
+    }
+
+    println("-------------------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+
+    println("-------------------")
+    val nomes2 = arrayOf("Maria", "Zazá", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
